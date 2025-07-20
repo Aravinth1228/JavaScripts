18a13f62f082680a4af5795678ad2cfcff932a57
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-07-20 23:18:57 +0530

    Add

---
+++ js/join().js
@@ -0,0 +1,6 @@
+//array.join(separator)
+const products = ["Pen", "Pencil", "Eraser", "Box"];
+console.log(products);
+
+console.log(products.join()); //Deafult , as Separator
+console.log(products.join('|')); // Pipe | as Separator
