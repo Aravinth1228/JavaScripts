213a1e2c538e93dc6753ba881c842d27fbaf4a15
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-07-22 23:49:24 +0530

    add

---
+++ js/Dot Notation and Bracket Notation/dot notation and bracket notation.js
@@ -0,0 +1,33 @@
+const user = {
+    name: "Tutor Joes",
+    age: 30,
+    job: "Developer"
+};
+
+// Dot notation
+console.log(user.name); // Output: "Tutor Joes"
+user.name = "Joes";
+console.log(user.name); // Output: "Joes"
+
+// Bracket notation
+console.log(user["age"]); // Output: 30
+user["age"] = 25;
+console.log(user["age"]); // Output: 25
+const prop = "name";
+console.log(user[prop]); // Output: "Joes"
+
+
+let user = {
+"first name": "Tutor",
+"last name": "Joes"
+};
+
+console.log(user["first name"]); // "Tutor"
+console.log(user["last name"]); // "Joes"
+
+
+
+
+
+let key = 'first name';
+console.log(user[key]); // "Tutor"
